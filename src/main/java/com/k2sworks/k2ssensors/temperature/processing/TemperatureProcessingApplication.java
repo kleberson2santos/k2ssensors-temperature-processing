f50550260b021f21bf2f1b06ae5bd0fcb4c4dbca
Kleberson Santos <kleberson2santos@gmail.com>
2025-04-27 17:40:31 -0400

    New project files

---
+++ src/main/java/com/k2sworks/k2ssensors/temperature/processing/TemperatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.k2sworks.k2ssensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k2sworks/k2ssensors/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k2sworks.k2ssensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
